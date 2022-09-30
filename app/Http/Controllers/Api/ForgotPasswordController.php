83e5ef8eba1f99125d224b8c34250b6a4d758970
roman <roman@transkontinent.net>
2022-09-30 18:47:20 +0300

    Add forgot password route API and send the mail

---
+++ app/Http/Controllers/Api/ForgotPasswordController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ForgotPasswordRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+class ForgotPasswordController extends Controller
+{
+    public function sendResetLinkEmail(ForgotPasswordRequest $request)
+    {
+        $response = $this->broker()->sendResetLink(
+            $request->only('email')
+        );
+
+        return $response == Password::RESET_LINK_SENT
+            ? $this->sendResetLinkResponse($request, $response)
+            : $this->sendResetLinkFailedResponse($request, $response);
+    }
+
+    public function broker()
+    {
+        return Password::broker();
+    }
+
+    protected function sendResetLinkResponse(Request $request, $response)
+    {
+        return response()->json(["message" => 'Email sent', 'response' => $response], 200);
+    }
+
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        return response()->json(["error" => 'Not such a user', 'response' => $response], 500);
+    }
+}
