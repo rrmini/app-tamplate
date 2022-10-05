0829c33f61743fd12937502ccafd988403e7edcd
roman <roman@transkontinent.net>
2022-10-05 10:33:44 +0300

    Add error notification in Register.vue

--- resources/js/store/user.js
+++ resources/js/store/user.js
@@ -3,8 +3,33 @@ import axios from "axios";
 const state = {
     isLoggedIn: false,
     userDetails: {},
+    errors: [],
 }
 const actions = {
+    registerUser(ctx, user) {
+        return new Promise((resolve) => {
+            axios
+                .get('sanctum/csrf-cookie')
+                .then(response => {
+                    axios
+                        .post('/api/register', user)
+                        .then(response => {
+                            if (response.data) {
+                                localStorage.setItem('x-token', response.config.headers['X-XSRF-TOKEN'])
+                                window.location.replace("/login")
+                            } else {
+                                reject( response )
+                            }
+                        })
+                        .catch((error) => {
+                            if (error.response.status === 422 ) {
+                                ctx.commit('setErrors', error.response.data.errors)
+                            } console.log(this.errors)
+                        })
+                })
+        })
+    },
+
     loginUser(ctx, payload) {
         return new Promise((resolve, reject) => {
             axios
@@ -69,12 +94,18 @@ const actions = {
 const mutations = {
     setLoggedIn(state, payload) {
         state.isLoggedIn = payload;
-    }
+    },
+    setErrors(state, invalidCredentials) {
+        state.errors = invalidCredentials
+    },
 }
 const getters = {
     loggedIn(state) {
         return state.isLoggedIn
-    }
+    },
+    errors(state) {
+        return state.errors
+    },
 }
 
 export default {
--- resources/js/views/auth/Register.vue
+++ resources/js/views/auth/Register.vue
@@ -5,27 +5,42 @@
                 <v-col cols="12" md="4" sm="8">
                     <v-card class="elevation-12">
                         <v-toolbar color="primary" dark flat>
-                            <v-toolbar-title>Login form</v-toolbar-title>
+                            <v-toolbar-title>Register form</v-toolbar-title>
                             <v-spacer/>
                         </v-toolbar>
+<!--                        -->
+                        <div v-if="validationErrors">
+                            <v-alert
+                                style="margin: 15px 0;"
+                                v-for="(value, index) in validationErrors"
+                                :key="index"
+                                prominent
+                                type="warning"
+                                variant="outlined"
+                                density="compact"
+                                closable
+                                close-label="Close Alert"
+                                color="red"
+                            >
+                                {{ value }}
+                            </v-alert>
+                        </div>
+
                         <v-card-text>
-                            <v-form :value="formValid" ref="registerForm">
+                            <v-form ref="registerForm">
                                 <v-text-field
-                                    :rules="emailRules"
                                     label="Email"
                                     name="email"
                                     type="email"
                                     v-model="user.email"
                                 />
                                 <v-text-field
-                                    :rules="nameRules"
                                     label="Name"
                                     name="name"
                                     type="text"
                                     v-model="user.name"
                                 />
                                 <v-text-field
-                                    :rules="passwordRules"
                                     id="password"
                                     label="Password"
                                     name="password"
@@ -33,7 +48,6 @@
                                     v-model="user.password"
                                 />
                                 <v-text-field
-                                    :rules="passwordRules"
                                     id="password_confirmation"
                                     label="Password Confirmation"
                                     name="password_confirmation"
@@ -50,35 +64,16 @@
                 </v-col>
             </v-row>
         </v-container>
-        <v-snackbar
-            v-model="registerSnackbar"
-            multi-line
-        >
-            {{ text }}
-
-            <template v-slot:actions>
-                <v-btn
-                    color="red"
-                    variant="text"
-                    @click="registerSnackbar = false"
-                >
-                    Close
-                </v-btn>
-            </template>
-        </v-snackbar>
     </div>
 </template>
 
 <script>
-import axios from "axios";
+import { mapActions, mapGetters } from 'vuex'
+
 export default {
     name: "Register",
     data() {
         return {
-            formValid: false,
-            registerSnackbar: false,
-            text: '',
-            color: '',
             user: {
                 email: '',
                 name: '',
@@ -87,26 +82,18 @@ export default {
             },
         }
     },
+    computed: {
+        ...mapGetters({
+            validationErrors: 'user/errors'
+        })
+    },
     methods: {
+        ...mapActions({
+            register: 'user/registerUser',
+        }),
         registerUser() {
             if(this.$refs.registerForm.validate()) {
-                axios
-                    .post('/api/register', this.user)
-                .then((response) => {
-                    if (response.data && response.data.success) {
-                        // this.registerSnackbar = true
-                        // this.text ='Success'
-                        // this.color=  'success'
-                        this.$router.push ({
-                            name: 'login'
-                        })
-                    }
-                })
-                .catch(() => {
-                    this.registerSnackbar = true
-                    this.text ='Failed'
-                    this.color=  'danger'
-                })
+                this.register( this.user)
             }
         },
     },
