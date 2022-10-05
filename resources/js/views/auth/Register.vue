28273477325b1db72e27aaa63028109dbaec2683
roman <roman@transkontinent.net>
2022-10-05 14:43:39 +0300

    makeup register page

--- resources/js/views/auth/Register.vue
+++ resources/js/views/auth/Register.vue
@@ -3,13 +3,15 @@
         <v-container class="fill-height" fluid>
             <v-row align="center" justify="center">
                 <v-col cols="12" md="4" sm="8">
-                    <v-card class="elevation-12">
-                        <v-toolbar color="primary" dark flat>
-                            <v-toolbar-title>Register form</v-toolbar-title>
-                            <v-spacer/>
-                        </v-toolbar>
-<!--                        -->
-                        <div v-if="validationErrors">
+                    <v-card class="elevation-12 my-4 pb-6">
+                        <div class="text-h4 pa-6">Sign Up</div>
+                        <div class="text-subtitle-1 pa-6 d-flex justify-space-between">
+                            <p>Already have an account?</p>
+<!--                            <v-btn variant="plain" color="secondary" @click="">Sign in-->
+                                <router-link :to="{name: 'login'}" class="text-decoration-none">Sign in</router-link>
+<!--                            </v-btn>-->
+                        </div>
+                        <div v-if="validationErrors" class="pa-6">
                             <v-alert
                                 style="margin: 15px 0;"
                                 v-for="(value, index) in validationErrors"
@@ -29,37 +31,60 @@
                         <v-card-text>
                             <v-form ref="registerForm">
                                 <v-text-field
-                                    label="Email"
-                                    name="email"
-                                    type="email"
-                                    v-model="user.email"
-                                />
-                                <v-text-field
+                                    variant="outlined"
+                                    clearable
                                     label="Name"
                                     name="name"
                                     type="text"
                                     v-model="user.name"
                                 />
                                 <v-text-field
+                                    variant="outlined"
+                                    clearable
+                                    label="Email"
+                                    name="email"
+                                    type="email"
+                                    v-model="user.email"
+                                />
+                                <v-text-field
+                                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                    :type="show1 ? 'text' : 'password'"
+                                    variant="outlined"
+                                    clearable
                                     id="password"
                                     label="Password"
                                     name="password"
-                                    type="password"
                                     v-model="user.password"
+                                    @click:append="show1 = !show1"
                                 />
                                 <v-text-field
+                                    :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                                    :type="show2 ? 'text' : 'password'"
+                                    variant="outlined"
+                                    clearable
                                     id="password_confirmation"
                                     label="Password Confirmation"
                                     name="password_confirmation"
-                                    type="password"
                                     v-model="user.password_confirmation"
+                                    @click:append="show2 = !show2"
                                 />
                             </v-form>
                         </v-card-text>
-                        <v-card-actions>
+                        <v-card-actions class="pa-5">
                             <v-spacer/>
-                            <v-btn @click="registerUser" color="primary">Register</v-btn>
+                            <v-btn
+                                block
+                                color="secondary"
+                                @click="registerUser"
+                                variant="outlined"
+                            >Sign Up</v-btn>
                         </v-card-actions>
+                        <div class="text-center mt-6">
+                            <div class="d-flex align-center justify-center gap">
+                                <v-btn class="bg-secondary mx-3"  icon="mdi-github" ></v-btn>
+                                <v-btn class="bg-secondary mx-3"  icon="mdi-vk" ></v-btn>
+                            </div>
+                        </div>
                     </v-card>
                 </v-col>
             </v-row>
@@ -74,6 +99,8 @@ export default {
     name: "Register",
     data() {
         return {
+            show1: false,
+            show2: false,
             user: {
                 email: '',
                 name: '',
@@ -96,6 +123,7 @@ export default {
                 this.register( this.user)
             }
         },
+
     },
 }
 </script>
